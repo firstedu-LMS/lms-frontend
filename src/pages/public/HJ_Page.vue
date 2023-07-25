1b7049aaf16834913917b421f35c5960624cb953
vaddshah <heinzwe2626@gmail.com>
2023-07-25 16:26:52 +0630

    HJD_page application form

--- src/pages/public/HJ_Page.vue
+++ src/pages/public/HJ_Page.vue
@@ -2,10 +2,10 @@
     <div class="px-2">
         <HPNavbar />
         <div v-for="career in careers " :key="career" class="shadow-md w-[90%] mx-auto my-6 px-6  py-4">
-            <h1 class="font-semibold text-lg">{{ career.name }}</h1>
+            <h1 class="text-lg font-semibold">{{ career.name }}</h1>
             <h6>Vacancy - {{ career.vacancy }}</h6>
             <h6>Position - {{ career.position }}</h6>
-            <router-link :to="{name : 'HJD_Page' , params : {id : career.id}}"  class="bg-blue-2 p-1 mt-2 text-white text-sm">Show Details</router-link>
+            <router-link :to="{name : 'HJD_Page' , params : {id : career.id}}"  class="p-1 mt-2 text-sm text-white bg-blue-2">Show Details</router-link>
         </div>
     </div>
 </template>
