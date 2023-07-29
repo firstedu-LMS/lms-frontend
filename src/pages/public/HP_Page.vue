731d4c4f5ca69298701a1294de080431ff1215ea
vaddshah <heinzwe2626@gmail.com>
2023-07-29 19:21:10 +0630

    creating contact component

--- src/pages/public/HP_Page.vue
+++ src/pages/public/HP_Page.vue
@@ -20,6 +20,11 @@
         <div class="px-2 sm:px-8">
             <HP04 class="my-12" />
         </div>
+
+        <!-- Contact Section -->
+        <div class="px-2 sm:px-8">
+            <HP05 class="my-12" />
+        </div>
     </div>
 </template>
 
@@ -29,9 +34,10 @@ import HPNavbar from '@/components/layouts/public/HPNavbar.vue';
 import HP02 from '@/components/public/HP_Page/HP02.vue';
 import HP03 from '@/components/public/HP_Page/HP03.vue';
 import HP04 from '@/components/public/HP_Page/HP04.vue';
+import HP05 from '@/components/public/HP_Page/HP05.vue';
     export default {
         components : {
-            HP01,HPNavbar,HP02,HP03,HP04
+            HP01,HPNavbar,HP02,HP03,HP04,HP05
         },
         data () {
             return {
