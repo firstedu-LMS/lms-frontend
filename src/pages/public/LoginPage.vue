f1e34d6d39d30bab3adacbe5d5a4e200338ee3d6
vaddshah <heinzwe2626@gmail.com>
2023-07-30 16:09:07 +0630

    Fix UI

--- src/pages/public/LoginPage.vue
+++ src/pages/public/LoginPage.vue
@@ -1,12 +1,40 @@
 <template>
     <div>
-        login
+        <HPNavbar/>
+        <div class="flex justify-center mt-6 sm:items-center sm:mx-20 sm:shadow-xl">
+            <img :src="image" class="hidden w-2/3 sm:block" alt="">
+            <form class="p-2 mb-24 sm:w-1/3">
+                <h1 class="text-2xl font-semibold text-center text-blue-2">Login Your Account</h1>
+                <div  class="px-4 py-2">
+                    <label for="" class="text-sm font-semibold ">Email</label>
+                    <input required type="email"  class="w-full mt-1 outline-none bg-transparent border px-2 py-0.5">
+                </div>
+                <div  class="px-4 py-2">
+                    <label for="" class="text-sm font-semibold ">Password</label>
+                    <input required type="password"  class="w-full mt-1 outline-none bg-transparent px-2 py-0.5 border">
+                </div>
+                <div  class="px-4 py-2 mt-4">
+                    <button class="w-full py-1 text-lg text-white bg-blue-2">Login</button>
+                </div>
+                <div  class="px-4 py-2 mt-4">
+                    <p class="">Doesn't have an account? <router-link class="text-blue-2" :to="{name : 'RegisterPage'}">Sign Up</router-link></p>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
+import HPNavbar from '@/components/layouts/public/HPNavbar.vue';
     export default {
-        
+        components : {
+            HPNavbar
+        },
+        data(){
+            return {
+                image : './images/layout/auth.jpg'
+            }
+        }
     }
 </script>
 
--- src/pages/public/RegisterPage.vue
+++ src/pages/public/RegisterPage.vue
@@ -1,12 +1,48 @@
 <template>
     <div>
-        register
+        <HPNavbar/>
+        <div class="flex justify-center mt-6 sm:items-center sm:mx-20 sm:shadow-xl">
+            <img :src="image" class="hidden w-2/3 sm:block" alt="">
+            <form class="p-4 sm:w-1/3">
+                <h1 class="text-2xl font-semibold text-center text-blue-2">Register Here</h1>
+                <div class="px-4 py-2">
+                    <label for="" class="text-sm font-semibold">Name</label>
+                    <input required type="text" class="w-full mt-1 outline-none bg-transparent border px-2 py-0.5">
+                </div>
+                <div  class="px-4 py-2">
+                    <label for="" class="text-sm font-semibold">Profile Picture</label>
+                    <input required type="file" class="w-full mt-1 outline-none bg-transparent border px-2 py-0.5 file:bg-transparent file:border-0">
+                </div>
+                <div  class="px-4 py-2">
+                    <label for="" class="text-sm font-semibold ">Email</label>
+                    <input required type="email"  class="w-full mt-1 outline-none bg-transparent border px-2 py-0.5">
+                </div>
+                <div  class="px-4 py-2">
+                    <label for="" class="text-sm font-semibold ">Password</label>
+                    <input required type="password"  class="w-full mt-1 outline-none bg-transparent px-2 py-0.5 border">
+                </div>
+                <div  class="px-4 py-2 mt-4">
+                    <button class="w-full py-1 text-lg text-white bg-blue-2">Register</button>
+                </div>
+                <div  class="px-4 py-2 mt-4">
+                    <p class="">Already have an account? <router-link class="text-blue-2" :to="{name : 'LoginPage'}">Sign In</router-link></p>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
+import HPNavbar from '@/components/layouts/public/HPNavbar.vue'
     export default {
-        
+        components : {
+            HPNavbar
+        },
+        data(){
+            return {
+                image : './images/layout/auth.jpg'
+            }
+        }
     }
 </script>
 
