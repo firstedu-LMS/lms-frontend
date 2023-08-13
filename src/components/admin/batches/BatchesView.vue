aafbf63909c07a0f3148cf944d9fba1ee3d9a26d
vadsha <heinzwe2626@gmail.com>
2023-08-13 16:00:31 +0630

    creating AdminBatchPage

---
+++ src/components/admin/batches/BatchesView.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <vue-good-table
+         :columns="columns"
+         :rows="batches"
+         :search-options="{enabled: true}"
+        >
+
+        </vue-good-table>
+    </div>
+</template>
+
+<script>
+import { VueGoodTable } from 'vue-good-table-next';
+    export default {
+        props : {
+            id : {
+                required : true
+            }
+        },
+        components : {
+            VueGoodTable
+        },
+        data() {
+            return {
+                batches : [],
+                columns : [
+                    {
+                        label : 'Batch',
+                        field : 'id'
+                    },
+                    {
+                        label : 'Instructor',
+                        field : 'instructor'
+                    },
+                    {
+                        label : 'Start-Date',
+                        field : 'start_date'
+                    },
+                    {
+                        label : 'End-Date',
+                        field : 'end_date'
+                    },
+                    {
+                        label : 'Start-Time',
+                        field : 'start_time'
+                    },
+                    {
+                        label : 'End-Time',
+                        field : 'end_time'
+                    },
+                    {
+                        label : 'Status',
+                        field : 'status'
+                    },
+                    {
+                        label : 'Actions',
+                        field : 'actions'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
