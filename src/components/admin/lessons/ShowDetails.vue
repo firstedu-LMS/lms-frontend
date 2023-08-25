932d61c36c6f90badfed66080ee0efd0ef819017
zawmyohtet <zmyohtet286@gmamil.com>
2023-08-25 22:37:40 +0630

    Question CRUD

--- src/components/admin/lessons/ShowDetails.vue
+++ src/components/admin/lessons/ShowDetails.vue
@@ -7,7 +7,72 @@
             </video>
             <h1 class="my-4 text-lg">{{ lesson.name }}</h1>
             <p v-html="lesson.description"></p>
-            <CreateQuestions/>
+
+            <div>
+                <button v-if="addQues" @click="showForm" class="bg-blue-2 px-3 py-1 mt-2 rounded text-white">Add Ques +</button>
+                <form v-if="quesForm" class="py-2" @submit.prevent="CreateQues">
+                    <button class="bg-blue-2 px-3 py-1 my-2 rounded text-white">Add Ques +</button>
+                    <div class="relative flex flex-wrap">
+                        <span class="flex px-3 py-1 items-center rounded-l border border-r-0 border-solid">
+                            Question
+                        </span>
+                        <input v-model="question.title" type="text" class="w-[84%] block rounded-r border border-gray-2 bg-transparent"/>
+                    </div>
+                    <div class="relative flex flex-wrap">
+                        <span class="flex px-3 py-1 items-center rounded-l border border-r-0 border-solid">
+                            Option-1
+                        </span>
+                        <input v-model="question.choice1" type="text" class="w-[85%] block rounded-r border border-gray-2 bg-transparent"/>
+                    </div>
+                    <div class="relative flex flex-wrap">
+                        <span class="flex px-3 py-1 items-center rounded-l border border-r-0 border-solid">
+                            Option-2
+                        </span>
+                        <input v-model="question.choice2" type="text" class="w-[85%] block rounded-r border border-gray-2 bg-transparent"/>
+                    </div>
+                    <div class="relative flex flex-wrap">
+                        <span class="flex px-3 py-1 items-center rounded-l border border-r-0 border-solid">
+                            Option-3
+                        </span>
+                        <input v-model="question.choice3" type="text" class="w-[85%] block rounded-r border border-gray-2 bg-transparent"/>
+                    </div>
+                    <div class="relative flex flex-wrap">
+                        <span class="flex px-4 py-1 items-center rounded-l border border-r-0 border-solid">
+                            Answer
+                        </span>
+                        <input v-model="question.answer" type="text" class="w-[85%] block rounded-r border border-gray-2 bg-transparent"/>
+                    </div>
+                </form>
+                <div>
+                    <div v-for="question in questions" :key="question" class="relative flex flex-wrap">
+                        <span class="flex px-3 py-1 items-center rounded-l border border-r-0 border-solid">
+                            Question
+                        </span>
+                        <input v-model="question.title" type="text" class="w-[84%] block rounded-r border border-gray-2 bg-transparent"/>
+           
+                            <span class="flex px-3 py-1 items-center rounded-l border border-r-0 border-solid">
+                                Option-1
+                            </span>
+                            <input v-model="question.choice1" type="text" class="w-[85%] block rounded-r border border-gray-2 bg-transparent"/>
+      
+                            <span class="flex px-3 py-1 items-center rounded-l border border-r-0 border-solid">
+                                Option-2
+                            </span>
+                            <input v-model="question.choice2" type="text" class="w-[85%] block rounded-r border border-gray-2 bg-transparent"/>
+                            <span class="flex px-3 py-1 items-center rounded-l border border-r-0 border-solid">
+                                Option-3
+                            </span>
+                            <input v-model="question.choice3" type="text" class="w-[85%] block rounded-r border border-gray-2 bg-transparent"/>
+                            <span class="flex px-4 py-1 items-center rounded-l border border-r-0 border-solid">
+                                Answer
+                            </span>
+                            <input v-model="question.answer" type="text" class="w-[85%] block rounded-r border border-gray-2 bg-transparent"/>
+                        <div class="w-[95%] flex justify-end">
+                            <button @click="deleteQues(question.id)" class="bg-red px-3 py-1 rounded my-1 text-white">Remove -</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -15,11 +80,8 @@
 <script>
 import ApiService from '@/services/ApiService';
 import filePath from '../../../services/public/filePath';
-import CreateQuestions from './CreateQuestions.vue';
     export default {
-        components: {
-            CreateQuestions
-        },
+
         props : {
             id : {
                 required  : true
@@ -27,8 +89,39 @@ import CreateQuestions from './CreateQuestions.vue';
         },
         data() {
             return {
+                addQues : true,
+                quesForm : false, 
+                questions : [],
+                question : {
+                    title : '',
+                    choice1 : '',
+                    choice2: '',
+                    choice3 : '',
+                    answer : '',
+                },
                 lesson : {},
                 filePath : filePath
+            }
+        },
+        methods : {
+            showForm(){
+                this.addQues = false
+                this.quesForm = true       
+            },
+            CreateQues(){
+                this.question.lesson_id = this.lesson.id
+                ApiService.post('admin/questions', this.question).then(() => {
+                  window.location.reload()  
+                }).catch((res) => {
+                    console.log(res);
+                })
+            },
+            deleteQues(id){
+                ApiService.delete(`admin/questions/${id}`).then(()=>{
+                    window.location.reload()
+                }).catch((res) => {
+                    console.log(res.data.data);
+                })
             }
         },
         mounted () {
@@ -36,6 +129,11 @@ import CreateQuestions from './CreateQuestions.vue';
                 this.lesson = res.data.data;
                 console.log(this.lesson);
                 this.$emit('setLesson' , this.lesson.name)
+            }).catch((res) => {
+                console.log(res);
+            })
+            ApiService.get(`admin/questions`).then((res) => {
+                this.questions = res.data.data
             }).catch((res) => {
                 console.log(res);
             })
