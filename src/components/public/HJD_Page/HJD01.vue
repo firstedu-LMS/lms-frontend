742278f156455a42ce75485bdd354d1763c76718
Freddy-Owin <kyawyelin022@gmail.com>
2023-10-12 16:28:05 +0630

    fixed app-form issue

--- src/components/public/HJD_Page/HJD01.vue
+++ src/components/public/HJD_Page/HJD01.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="mt-6 sm:flex">
-
-        <div class="sm:w-2/3">
+        <div class="sm:w-[75%]">
             <h1 class="mb-6 text-3xl font-bold text-black">{{ career.name }}</h1>
             <div class="my-6">
                 <h1 class="my-2 text-2xl">Job Description</h1>
@@ -12,8 +11,7 @@
                 <p class="ml-4" v-html="career.job_requirement"></p>
             </div>
         </div>
-
-        <ul class="m-10 mb-0 text-center sm:w-1/4 sm:mt-0">
+        <ul class="sm:ml-10 text-center sm:w-[25%] sm:mt-0">
             <li class="text-white bg-green">Summary</li>
             <li>Vacnacy - {{ career.vacancy }} posts</li>
             <li>Position - {{ career.position }}</li>
@@ -28,7 +26,6 @@
                 <button @click="showForm" class="w-full">Apply Now</button>
             </li>
         </ul>
-
     </div>
 </template>
 
@@ -48,8 +45,10 @@ import axios from 'axios'
         },
         mounted(){
             axios.get(`careers/${this.id}`).then(res => {
-                this.career = res.data.data
-                this.$emit('name' , this.career.name)
+                this.career = res.data.data;
+                res.data.data.deadline= new Date(res.data.data.deadline).toDateString();
+                res.data.data.created_at = new Date(res.data.data.created_at).toDateString();
+                this.$emit('name' , this.career.name);
             }).catch(res => {
                 console.log(res);
             })
@@ -72,6 +71,6 @@ import axios from 'axios'
  li{
         padding: 0.5rem 2rem;
         text-align: center;
-        border: 1px solid gray;
+        border: 1px solid rgb(187, 187, 187);
     }
 </style>
