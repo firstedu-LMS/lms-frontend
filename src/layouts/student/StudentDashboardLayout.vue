abfcba09ac5a6b87aaacf0c0793cf2206906cebf
vadsha <heinzwe2626@gmail.com>
2023-10-27 16:22:46 +0630

    student dashboard layout

--- src/layouts/student/StudentDashboardLayout.vue
+++ src/layouts/student/StudentDashboardLayout.vue
@@ -1,45 +1,29 @@
 <template>
-  <div class=" bg-[#eae8e8]">
-      <nav :class="SideBar ? 'w-full' : ''" class="transition sticky top-0 z-50 flex justify-between px-4 py-1 bg-green">
-        <button @click="SideBar = !SideBar" class="block mx-3 mt-2 text-white">
-          <span style="font-size : 30px;" class="material-icons-sharp">menu</span>
-        </button>
-        <button class="text-white">
-          <span style="margin: auto 0px; margin-right: 20px; font-size : 30px;" class="material-icons-sharp">house</span>
-          <span style="margin: auto 0px; font-size : 30px;" class="material-icons-sharp">notifications</span>
-        </button>
-        <button class="sm:hidden">
-          <span style="margin: auto 0px;" class="material-icons-sharp">house</span>
-          <span style="margin: auto 0px;" class="material-icons-sharp">notifications</span>
-        </button>
+  <div class="flex justify-end">
+    <aside :class="sideBar ? 'sm:w-1/6 w-1/2' : 'w-[60px]'" class="h-screen fixed top-0 left-0 border-r border-[#e2e8f0] z-50 bg-white pr-[12px] py-2">
+      <button @click="sideBar = !sideBar" style="padding-left: 15px;">
+        <span class="material-icons-outlined">menu</span>
+      </button>
+      <ul>
+        <li class="my-5" v-for="item in sideItems" :key="item.name">
+          <router-link :to="{name : item.path}" class="flex items-center">
+            <span class="material-icons-sharp" style="margin-right: 8px; background-color: rgb(249 250 251 / var(--tw-bg-opacity));z-index: 1; padding-left: 15px;">{{ item.icon }}</span>
+            <Transition>
+              <span v-if="sideBar" style="z-index: 0.5;">{{ item.name }}</span>
+            </Transition>
+          </router-link>
+        </li>
+      </ul>
+    </aside>
+ <!-- :class="sideBar ? 'sm:w-5/6 w-screen' : 'w-[calc(100vw_-_60px)]'" -->
+    <div  :class="sideBar ? 'sm:w-5/6 w-[calc(100vw_-_60px)]' : 'w-[calc(100vw_-_60px)]'"  class="transition-all ease-in-out">
+      <nav class="sticky top-0 flex justify-end w-full p-2 text-white shadow-lg bg-green">
+        <span class="material-icons-sharp">home</span>
+        <span style="margin: 0px 12px;" class="material-icons-sharp">home</span>
       </nav>
-      <div class="flex justify-between">
-        <aside class="sticky top-0 hidden h-screen sm:block" :class="SideBar ? 'w-1/6' : 'w-1/12'">  
-          <div>
-            <ul class="mt-4">
-              <li class="py-2 my-4" v-for="item in sideItems" :key="item.name">
-                <router-link :to="{name : item.path}" class="flex items-center hover:text-blue-2">
-                  <span style="background-color: #eae8e8; z-index: 1;width: 50px;text-align: end;margin-right: 5px;" class="material-icons-outlined">{{ item.icon }}</span>
-                  <Transition name="SideBar">
-                    <span style="z-index: 0.1; width: 30px;" v-if="SideBar">{{ item.name }}</span>
-                  </Transition>
-                </router-link>
-              </li>
-              <li class="py-2 font-bold my-4 border-t border-gray-2 text-red">
-                <button @click="logout" class="flex w-full items-center">
-                  <span  style="background-color: #eae8e8; z-index: 1;width: 50px;text-align: end;margin-right: 5px;"  class="material-icons-outlined">logout</span> 
-                  <Transition name="SideBar">
-                    <span style="z-index: 0.1;" v-if="SideBar">Logout</span>
-                  </Transition>
-                </button>
-              </li>
-            </ul>
-          </div>
-        </aside>
-        <main style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;"  :class="SideBar ? 'w-5/6' : 'w-11/12'" class="z-10 mx-4 mt-4 bg-white rounded-2xl">
-            <router-view></router-view>
-        </main>
-      </div>
+      <RouterView />
+    </div>
+
   </div>
 </template>
 
@@ -49,8 +33,7 @@ import { useAuthStore } from '@/stores/auth';
 export default {
   data() {
     return {
-      btn: true,
-      SideBar: false,
+      sideBar: false,
       authStore : useAuthStore(),
       sideItems: [
         {
@@ -84,7 +67,6 @@ export default {
   methods: {
     showSidebar() {
       this.sideBar = true;
-      this.btn = false;
     },
     logout(){
         ApiService.post('logout').then(()=>{  
